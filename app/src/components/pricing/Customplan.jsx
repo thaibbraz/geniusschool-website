a7cc2565039db89a3771b8db93c721298c621adc
Giulia Cellerino <giulia.cellerino@icloud.com>
2024-07-23 16:24:49 +0200

    Custom plan done

--- app/src/components/pricing/Customplan.jsx
+++ app/src/components/pricing/Customplan.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import vector from "../../images/vector.png";
 
 const Customplan = () => {
-  return <div className="customplan">Customplan</div>;
+  return (
+    <div className="customplan">
+      <img src={vector} alt="vector-image" className="customplan-vector" />
+      <div className="customplan-box">
+        <div className="customplan-box-line"></div>
+        <div className="customplan-box-title title">Custom</div>
+      </div>
+      <div className="customplan-box-text subheading">
+        Looking for a custom plan?
+      </div>
+      <div>
+        <a
+          href="mailto:geniusschool@geniusschool.com?subject=Requesting%20a%20Custom%20Plan"
+          className="customplan-box-text-request subheading"
+        >
+          REQUEST A QUOTE
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Customplan;
